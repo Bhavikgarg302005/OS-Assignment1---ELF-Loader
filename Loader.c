35dcc58826e79e0e8b7bfefb2856f06c8e9ddd81
Bhavikgarg302005 <147907427+Bhavikgarg302005@users.noreply.github.com>
2024-08-29 20:13:48 +0530

    Update Loader.c

--- Loader.c
+++ Loader.c
@@ -42,21 +42,21 @@ void load_and_run_elf(char** exe) {
     exit(1);
   }
   
-  int l=lseek(fd,ehdr->phoff,SEEK_SET);
-  if(l==-1){
-     perror("Error");
+  int l=lseek(fd,ehdr->e_phoff,SEEK_SET);
+  if(l<0){
+     perror("Error in moving to offset");
      loader_cleanup();
      exit(1);
   }
   // 2. Iterate through the PHDR table and find the section of PT_LOAD type that contains the address of the entrypoint method in fib.c
   //read Pheader;
-  phdr=(Elf32_Phdr *)malloc(ehdr->phnum*ehdr->e_phentsize);
+  phdr=(Elf32_Phdr *)malloc(ehdr->e_phnum*ehdr->e_phentsize);
+  int size2=ehdr->e_phnum*ehdr->e_phentsize;
   if(phdr==NULL){
    perror("Space Allocation not done for program header table");
    loader_cleanup();
    exit(1);
   }
-  int size2=ehdr.phnum*e_phentsize;
   int read2=read(fd,phdr,size2);
   if(read2!=size2){
     perror("Cannnot read properly program header table");
@@ -67,29 +67,29 @@ void load_and_run_elf(char** exe) {
   // 4. Navigate to the entrypoint address into the segment loaded in the memory in above step
   // 5. Typecast the address to that of function pointer matching "_start" method in fib.c.
   // 6. Call the "_start" method and print the value returned from the "_start"
-  int add_start=ehdr->e_entry;
-  bool flag=false;
-  for(int i=0;i<ehdr->phnum;i++){
-    if(phdr[i]->p_type==PT_LOAD){
-     int start=phdr[i]->p_vaddr;
-     int end=start+phdr[i]->p_memsz;
-     if(start<=add_start && end>=add_start){
-      flag=true;
+  for(int i=0;i<ehdr->e_phnum;i++){
+    if(phdr[i].p_type==PT_LOAD){
+     void *virtual_mem=mmap((void *)phdr[i].p_vaddr, phdr[i].p_memsz, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS ,0 ,0);
+     if(virtual_mem == MAP_FAILED){
+      perror("Cannot load data in memory");
+      loader_cleanup();
+      exit(1);
+     }
+     lseek(fd,phdr[i].p_offset,SEEK_SET);
+     int size3=phdr[i].p_memsz;
+     int read3=read(fd,virtual_mem,size3);
+     if(size3!=read3){
+       perror("Segment cannot be loaded properly");
+       loader_cleanup();
+       exit(1);
      }
     }
   }
   
-  if(flag){
-     int (*_start)(void) = (int (*)(void))(ehdr->e_entry);
-     int result = _start();
-     printf("User _start return value = %d\n",result);
-  }
-  else{
-   printf("Address Not found\n");
-   loader_cleanup();
-   exit(1);
-  }
   
+  int (*_start)(void) = (int (*)(void))(ehdr->e_entry);
+  int result = _start();
+  printf("User _start return value = %d\n",result);
   loader_cleanup();
 }
 
