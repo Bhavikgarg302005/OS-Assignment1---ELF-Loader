113c10d1b6e84b26e814a99e97cf6b56df724e3f
Bhavikgarg302005 <147907427+Bhavikgarg302005@users.noreply.github.com>
2024-08-30 15:24:30 +0530

    Update Loader.c

--- Loader.c
+++ Loader.c
@@ -24,75 +24,86 @@ void loader_cleanup() {
  */
 void load_and_run_elf(char** exe) {
   fd = open(exe[1], O_RDONLY);
-  // 1. Load entire binary content into the memory from the ELF file.
+  //1.Load entire binary content into the memory from the ELF file.
+  //2.checking error in opening executable file:
   if(fd<0){
    perror("Error in opening the executable file");
    loader_cleanup();
    exit(1);
   }
+  //3.Allocating size to elf header:
   int size1=sizeof(Elf32_Ehdr);
   ehdr=(Elf32_Ehdr *)malloc(size1);
+  //4.Checking error that allocation of space has been done properly or not:
   if(ehdr==NULL){
     perror("Space Allocation not done for elf header");
     loader_cleanup();
     exit(1);
   }
+  //5.reading Elf header to get offset of Phdr as well as e_entry:
   int read1=read(fd,ehdr,size1);
+  //6.checking wheather reading is done properly or not:
   if(read1!=size1){
     perror("Cannnot read properly elf header");
     loader_cleanup();
     exit(1);
   }
-  
+  //7.Moving fd to phdr offset so that we can read phdr table properly:
   int l=lseek(fd,ehdr->e_phoff,SEEK_SET);
+  //8.Cheking wheather movement of fd is done or not properly:
   if(l<0){
      perror("Error in moving to offset");
      loader_cleanup();
      exit(1);
   }
-  // 2. Iterate through the PHDR table and find the section of PT_LOAD type that contains the address of the entrypoint method in fib.c
-  //read Pheader;
+  //9.Allocating space to Phdr table:
   phdr=(Elf32_Phdr *)malloc(ehdr->e_phnum*ehdr->e_phentsize);
   int size2=ehdr->e_phnum*ehdr->e_phentsize;
+  //10.Checking error that allocation of space has been done properly or not:
   if(phdr==NULL){
    perror("Space Allocation not done for program header table");
    loader_cleanup();
    exit(1);
   }
+  //11.reading phdr table:
   int read2=read(fd,phdr,size2);
+  //12.has reading done properly or not:
   if(read2!=size2){
     perror("Cannnot read properly program header table");
     loader_cleanup();
     exit(1);
   }
-  // 3. Allocate memory of the size "p_memsz" using mmap function and then copy the segment content
-  // 4. Navigate to the entrypoint address into the segment loaded in the memory in above step
-  // 5. Typecast the address to that of function pointer matching "_start" method in fib.c.
-  // 6. Call the "_start" method and print the value returned from the "_start"
+  //13.Iterate through the PHDR table and find the section of PT_LOAD type that contains the address of the entrypoint method in fib.c
   for(int i=0;i<ehdr->e_phnum;i++){
     if(phdr[i].p_type==PT_LOAD){
+     //14.Allocate memory of the size "p_memsz" using mmap function and then copy the segment content
      void *virtual_mem=mmap((void *)phdr[i].p_vaddr, phdr[i].p_memsz, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS ,0 ,0);
+     //15.Error checking is space allocated correctly:
      if(virtual_mem == MAP_FAILED){
       perror("Cannot load data in memory");
       exit(1);
      }
+     //16.Moving fd to each phdr[i].offset so as to copy the segment:
      int xyz=lseek(fd,phdr[i].p_offset,SEEK_SET);
+     //17.Error in moving fd to phdr[i].offset
      if(xyz==-1){
       perror("Error in reading");
       loader_cleanup();
       exit(1);
      }
+     //18.coping the segment 
      int size3=phdr[i].p_memsz;
      int read3=read(fd,virtual_mem,size3);
+     //19.Error checking 
      if(read3<0){
        perror("Segment cannot be loaded properly");
        exit(1);
      }
     }
   }
-  
-  
+  //20.Typecasting the address to that of function pointer matching "_start" method in fib.c.
   int (*_start)(void) = (int (*)(void))(ehdr->e_entry);
+  //21.Calling the "_start" method and printing the value returned from the "_start"
   int result = _start();
   printf("User _start return value = %d\n",result);
   loader_cleanup();
