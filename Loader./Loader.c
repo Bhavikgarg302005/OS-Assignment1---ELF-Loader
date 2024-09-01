9ba259743ddc61018c3caba2048553dce0e876d0
Bhavikgarg302005 <147907427+Bhavikgarg302005@users.noreply.github.com>
2024-09-02 00:28:57 +0530

    Update Loader.c

--- Loader./Loader.c
+++ Loader./Loader.c
@@ -115,16 +115,3 @@ void load_and_run_elf(char** exe) {
   loader_cleanup();
 }
 
-int main(int argc, char** argv) 
-{
-  if(argc != 2) {
-    printf("Usage: %s <ELF Executable> \n",argv[0]);
-    exit(1);
-  }
-  // 1. carry out necessary checks on the input ELF file
-  // 2. passing it to the loader for carrying out the loading/execution
-  load_and_run_elf(argv);
-  // 3. invoke the cleanup routine inside the loader  
-  loader_cleanup();
-  return 0;
-}
